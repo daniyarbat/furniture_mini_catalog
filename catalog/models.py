ea00b4c93c30b1df98f4bde32e96c027672e235e
Daniyar Batyrbaev <batyrbaev_daniyar@mail.ru>
2023-12-19 17:30:00 +0600

    ADD: added Product and Category models and fields

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.CharField(max_length=250, **NULLABLE, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Продукт')
+    description = models.TextField(max_length=250, verbose_name='Описание')
+    photo = models.ImageField(upload_to='photos/', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.IntegerField(**NULLABLE, verbose_name='Цена')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
