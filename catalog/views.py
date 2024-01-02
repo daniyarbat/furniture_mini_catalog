0d0826dad7f1a2e0dafbf2eb1d58df833d223800
Daniyar Batyrbaev <batyrbaev_daniyar@mail.ru>
2024-01-02 23:03:49 +0600

    ADD: changed catalog app from fbv to cbv

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
-from catalog.models import Category, Product, Contact
+from django.views.generic import ListView, DetailView
+from catalog.models import Product, Contact
 
 
 def index(request):
     product_list = Product.objects.all().order_by('pk')
-    paginator = Paginator(product_list, 3)
+    paginator = Paginator(product_list, 6)
     page_number = request.GET.get("page")
     page_object = paginator.get_page(page_number)
     context = {
@@ -24,39 +25,31 @@ def show_item(request, product_pk):
     return render(request, 'catalog/item.html', context)
 
 
-def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        print(f'{name} ({email}): {message}')
-
-    contact_list = [{
-        'name': contact.name,
-        'phone': contact.phone,
-        'email': contact.email
-    } for contact in Contact.objects.all()]
-
-    context = {
-        'title': 'Контакты',
-        'contacts': contact_list
-    }
-
-    return render(request, "catalog/contacts.html", context)
+class ProductList(ListView):
+    paginate_by = 6
+    model = Product
+    template_name = 'catalog/index.html'
+    extra_context = {'title': 'Мягкая мебель'}
 
 
-# def categories(request):
-#     context = {
-#         'object_list': Category.objects.all(),
-#         'title': 'Мягкая мебель: категории'
-#     }
-#     return render(request, 'catalog/categories.html', context)
-#
-#
-# def category_items(request, pk):
-#     category_item = Category.objects.get(pk=pk)
-#     context = {
-#         'object_list': Product.objects.filter(category_id=pk),
-#         'title': f'Мягкая мебель: {category_item.name}'
-#     }
-#     return render(request, 'catalog/products.html', context)
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'catalog/item.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(id=self.kwargs.get('pk'))
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+        item = Product.objects.get(pk=self.kwargs.get('pk'))
+        context_data['item'] = item
+        context_data['title'] = item
+        return context_data
+
+
+class ContactsView(ListView):
+    model = Contact
+    template_name = 'catalog/contacts.html'
+    extra_context = {'title': 'Контакты'}
