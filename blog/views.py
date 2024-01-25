c25ca6915f6698287b4a288c707d86ce2308e188
Daniyar Batyrbaev <batyrbaev_daniyar@mail.ru>
2024-01-25 21:05:45 +0600

    ADD: additional task 23.1

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import redirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
@@ -5,16 +6,18 @@ from django.views.generic import CreateView, ListView, DetailView, UpdateView, D
 from blog.models import Article
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(PermissionRequiredMixin, CreateView):
     model = Article
     fields = ('title', 'content', 'preview', 'is_published',)
     success_url = reverse_lazy('blog:blog')
+    permission_required = 'blog.add_article'
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(PermissionRequiredMixin, UpdateView):
     model = Article
     fields = ('title', 'content', 'preview', 'is_published',)
     success_url = reverse_lazy('blog:view_article')
+    permission_required = 'blog.change_article'
 
     def form_valid(self, form):
         if form.is_valid():
@@ -48,6 +51,12 @@ class ArticleDetailView(DetailView):
         return obj
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(PermissionRequiredMixin, DeleteView):
     model = Article
     success_url = reverse_lazy('blog:blog')
+    permission_required = 'blog.delete_article'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Удаление "{self.object.title}"'
+        return context
